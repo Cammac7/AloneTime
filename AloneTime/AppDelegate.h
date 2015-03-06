f1aae7ef4f6a660fc1d714c3c50ad2a731736850
Cameron MacArthur <acousticguitar710@gmail.com>
2015-03-05 22:27:53 -0600

    Initial Commit

---
+++ AloneTime/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AloneTime
+//
+//  Created by Cameron MacArthur on 3/5/15.
+//  Copyright (c) 2015 Cameron MacArthur. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AloneTime/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AloneTime
+//
+//  Created by Cameron MacArthur on 3/5/15.
+//  Copyright (c) 2015 Cameron MacArthur. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
