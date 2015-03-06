9d039f56294443a181f60b0f2079c785eadfb9c8
rahul-matta <rahulmatta2016@u.northwestern.edu>
2015-03-05 22:57:02 -0600

    added signInclass

---
+++ AloneTime/SignInViewController.h
@@ -0,0 +1,14 @@
+//
+//  SignInViewController.h
+//  AloneTime
+//
+//  Created by Rahul Matta on 3/5/15.
+//  Copyright (c) 2015 Cameron MacArthur. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SignInViewController : UIViewController
+
+@property (strong, nonatomic) IBOutlet UIButton *signInButton;
+@end
